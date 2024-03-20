2f7089a53ccf98de6bfa213ee916261290a9e59c
jalal-codr <abduljalal849@gmail.com>
2024-03-20 20:52:33 +0100

    update to auth in profile page

--- Screens/Profile/Profile.jsx
+++ Screens/Profile/Profile.jsx
@@ -2,10 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native';
 import { Input,Layout,Text} from '@ui-kitten/components';
 import { StyleSheet } from 'react-native';
-import SignIn from '../SignIn/SignIn';
+import{onAuthStateChanged} from 'firebase/auth'
+import { auth } from '../../FirbaseConfig';
 
 function Profile({navigation}) {
   const[user,setUser]=useState();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+    });
+
+
+    // Clean up subscription on unmount
+    return () => unsubscribe();
+}, []);
 
 
   useEffect(()=>{
