7e7d1dc7025f4e7b14745f7e0623d36141791af8
jalal-codr <abduljalal849@gmail.com>
2024-03-23 10:22:08 +0100

    render function added

--- Screens/Home/Home.jsx
+++ Screens/Home/Home.jsx
@@ -1,12 +1,45 @@
 import React, { useState } from 'react'
-import {Image,View,StyleSheet,TouchableOpacity} from 'react-native'
+import {Image,View,StyleSheet,TouchableOpacity, Alert, Button} from 'react-native'
 import { ApplicationProvider, Layout,Text,SafeAreaProvider } from '@ui-kitten/components';
 import checkList from '../../assets/Checklist.png'
 import logo from '../../assets/icon.png'
 import AntDesign from 'react-native-vector-icons/AntDesign'
+import Form from '../../Components/Tasks/Form';
 
 function Home() {
   const plusIcon = () => <AntDesign color='white' name="pluscircle" size={45}  />;
+
+  const [check,setCheck] = useState(false)
+
+
+
+  const plusBtnClick = ()=>{
+    setCheck(true)
+  }
+
+  const render = ()=>{
+    if(check){
+      return(<>
+      <Text>Hello</Text>
+      </>)
+    }else{
+      return(<>
+          <View style={styles.photoView}>
+          <Image source={checkList}></Image>
+          <Text style={styles.h_Text} >What do you want to do today ?</Text>
+          <Text style={styles.p_text}>Tap the + to add a new task</Text>
+          </View>
+          <View style={styles.plusIcon} >
+            <TouchableOpacity onPress={plusBtnClick}>
+            {
+              plusIcon()
+            }
+            </TouchableOpacity>
+          </View>
+      </>)
+    }
+  }
+
    
 
   return (
@@ -15,19 +48,10 @@ function Home() {
         <Text style={styles.text}>Hi Jalal</Text>
         <Image style={styles.userLogo} source={logo}></Image>
       </View>
+      {
+        render()
+      }
 
-      <View style={styles.photoView}>
-      <Image source={checkList}></Image>
-      <Text style={styles.h_Text} >What do you want to do today ?</Text>
-      <Text style={styles.p_text}>Tap the + to add a new task</Text>
-      </View>
-      <View style={styles.plusIcon} >
-        <TouchableOpacity>
-        {
-          plusIcon()
-        }
-        </TouchableOpacity>
-      </View>
     </Layout>
   )
 }
